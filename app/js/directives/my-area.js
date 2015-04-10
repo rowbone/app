8b23c24ad404fba47426e9d22663ec883cd17352
wulujunwlj <wulujunwlj@163.com>
2015-04-10 18:10:58 +0800

    增加my-area

---
+++ app/js/directives/my-area.js
@@ -0,0 +1,12 @@
+'use strict';
+
+app.directive('myArea', [function() {
+	// 
+	return {
+		restrict: 'AE',
+		templateUrl: './views/demo/conow-area/area.html',
+		link: function(scope, elem, attrs) {
+			// 
+		}
+	}
+}]);
