9b605fe064bb997f7c2469d9c096fdbc8fbde61f
wulujunwlj <wulujunwlj@163.com>
2015-05-05 23:17:10 +0800

    通讯录找人模块，添加关注功能

--- app/views/business/home/contactlist/contact-list-phone2.js
+++ app/views/business/home/contactlist/contact-list-phone2.js
@@ -37,6 +37,7 @@ app.controller('CollectionCtrl', ['$scope',
 			options.collectionNone = true;
 		}
 
+		// 添加收藏
 		$scope.addCollection = function() {
 			var iTabsLen = $scope.contactListTabs.length;
 			for(var i=0; i<iTabsLen; i++) {
@@ -47,23 +48,31 @@ app.controller('CollectionCtrl', ['$scope',
 	}
 ]);
 
-app.controller('PersonSearchCtrl', ['$scope', '$http', 
-	function($scope, $http) {
+app.controller('PersonSearchCtrl', ['$scope', '$http', '$timeout', '$modal', 
+	function($scope, $http, $timeout, $modal) {
 		var entity = $scope.entity = {};
+		var options = $scope.options = {
+			search: false
+		};
 		var personSearchUrl = 'data/bz/home/contactlist2/staffInfo-queryStaffInfoByNameByPage.json';
 
 		entity.searchType = 'name';
 
+		// 搜索框 enter 触发搜索事件
 		$scope.enterPress = function(e) {
 			var params = {};
 			if(e.keyCode === 13) {
+				options.search = true;
+
 				params.searchKey = entity.searchKey;
 				params.searchType = entity.searchType;
 
 				$http.get(personSearchUrl)
 					.success(function(data, status, headers, config) {
 						console.log(data);
-						entity.searchResults = data.obj;
+						$timeout(function() {
+							entity.searchResults = data.obj;
+						}, 2000);
 					})
 					.error(function(data, status, headers, config) {
 						console.log('Get ' + personSearchUrl + ' wrong...');
@@ -72,12 +81,38 @@ app.controller('PersonSearchCtrl', ['$scope', '$http',
 
 			e.stopPropagation();
 		};
+		// 添加关注确认
+		$scope.collectionConfirm = function(user) {
+console.log(user);
+			var modalInstance = $modal.open({
+				templateUrl: 'views/business/home/contactlist/confirm.html',
+				controller: 'ConfirmCtrl'
+			});
+
+			modalInstance.result.then(function(rtnVal) {
+				console.log(rtnVal);
+			}, function(rtnVal) {
+				console.log(rtnVal)
+			})
+		};
 
 		$scope.$watch('entity.searchKey', function(newVal) {
 			// console.log(newVal);
 		});
 	}
 ]);
+
+app.controller('ConfirmCtrl', ['$scope', '$modalInstance', 
+	function($scope, $modalInstance) {
+		$scope.confirm = function() {
+			$modalInstance.close('confirm');
+		};
+
+		$scope.cancel = function() {
+			$modalInstance.close('cancel');
+		};
+	}
+])
 
 app.controller('OrgSearchCtrl', ['$scope', 
 	function($scope) {
