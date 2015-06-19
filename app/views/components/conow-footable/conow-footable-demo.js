102de1296898174fcb5ee53bf24582f602e9a05f
wulujunwlj <wulujunwlj@163.com>
2015-06-19 18:10:53 +0800

    update footable

--- app/views/components/conow-footable/conow-footable-demo.js
+++ app/views/components/conow-footable/conow-footable-demo.js
@@ -11,24 +11,28 @@ app.controller('footableCtrl', ['$scope', '$http', 'conowModals',
 	
 	$scope.selected = 
 		[{
-	     "TITLE": "请假审批",
-	     "date":"2014-12-2 12:20",
-	     "avatar": "img/person/person_photo_2.png",
-	     "content":"有请假流程到你审批环节",
-	     "url":"app.oa.chat"
-	   }, {
-	       "TITLE": "区文辉",
-	       "date":"2014-10-1 12:20",
-	       "avatar": "img/person/person_photo_3.png",
-	       "content":"XXXXXXXX",
-	       "url":"app.oa.chat"
-	     }, {
-			           "TITLE": "通知审批",
-			           "date":"2014-12-1 12:20",
-			           "avatar": "img/person/person_photo_3.png",
-			           "content":"有待审批的通知需要处理",
-			           "url":"app.oa.noticeAudit"
-			         }];
+				"ID": "3",
+		    "TITLE": "请假审批",
+		    "date": "2014-12-2 12:20",
+		    "avatar": "img/person/person_photo_2.png",
+		    "content": "有请假流程到你审批环节",
+		    "url": "app.oa.chat"
+		}, {
+				"ID": "",
+		    "TITLE": "区文辉",
+		    "date": "2014-10-1 12:20",
+		    "avatar": "img/person/person_photo_3.png",
+		    "content": "XXXXXXXX",
+		    "url": "app.oa.chat"
+		}, {
+				"ID": "",
+		    "TITLE": "通知审批",
+		    "date": "2014-12-1 12:20",
+		    "avatar": "img/person/person_photo_3.png",
+		    "content": "有待审批的通知需要处理",
+		    "url": "app.oa.noticeAudit"
+		}];
+
 	
 	$http.get('views/components/conow-footable/data/footable-list.json')
 		.success(function(data, status, config, header) {
