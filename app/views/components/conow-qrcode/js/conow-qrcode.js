2a1c013b8463d80d6aea506e138dcd96f2bdd424
wulujunwlj <wulujunwlj@163.com>
2015-07-07 21:17:36 +0800

    update qrcode

---
+++ app/views/components/conow-qrcode/js/conow-qrcode.js
@@ -0,0 +1,80 @@
+'use strict';
+
+// filter to convert object data to vcard string
+app.filter('vcardFormat', [
+	function() {
+		return function(input, params) {
+			var vcardPrefix = 'BEGIN:VCARD,',
+					vcardArr = [],
+					vcardSuffix = ',END:VCARD',
+					vcardStr = '';
+			// prefix
+			vcardArr.push(vcardPrefix);
+			// content
+			vcardArr.push('N:' + input.NAME);
+			vcardArr.push('ORG:' + input.ORG);
+			vcardArr.push('TITLE:' + input.TITLE);
+			vcardArr.push('TEL;type=CELL:' + input.CELLPHONE);
+			vcardArr.push('TEL;type=WORK:' + input.PHONE);
+			vcardArr.push('EMAIL;type=INTERNET;type=WORK:' + input.EMAIL);
+			// suffix
+			vcardArr.push(vcardSuffix);
+
+			vcardStr = vcardArr.join(String.fromCharCode(13) + String.fromCharCode(10));
+
+			return vcardStr;
+		}
+	}
+]);
+
+// conow-qrcode directive
+app.directive('conowQrcode', ['DataService', '$filter', 
+	function(DataService, $filter) {
+		return {
+			restrict: 'AE',
+			template: '<div class="conow-qrcode"></div>',
+			scope: {
+				text: '='
+			}, 
+			link: function(scope, elem, attrs) {
+				console.log(scope.text);
+				console.log(attrs.text);
+
+				scope.$watch(scope.text, function(newVal, oldVal) {
+					console.log('newVal-->', newVal);
+				});
+				
+				var $qrcode = elem.find('.conow-qrcode');
+				var qrcodeDom = $qrcode[0];
+				var qrcode = new QRCode(qrcodeDom, {
+					text: scope.text,
+				  // width: 128,
+				  // height: 128,
+				  colorDark : "#000000",
+				  colorLight : "#ffffff",
+				  correctLevel : QRCode.CorrectLevel.H
+				});
+
+				// DataService.getData('views/components/conow-qrcode/data/qrcode.json')
+				// 	.then(function(data) {
+				// 		var text = $filter('vcardFormat')(data.obj);
+												
+				// 		var $qrcode = elem.find('.conow-qrcode');
+				// 		var qrcodeDom = $qrcode[0];
+				// 		var qrcode = new QRCode(qrcodeDom, {
+				// 			text: text,
+				// 		  // width: 128,
+				// 		  // height: 128,
+				// 		  colorDark : "#000000",
+				// 		  colorLight : "#ffffff",
+				// 		  correctLevel : QRCode.CorrectLevel.H
+				// 		});
+
+				// 	}, function(msg) {
+				// 		console.error('msg-->', msg);
+				// 	});
+
+			}
+		};
+	}
+]);
--- app/views/components/conow-qrcode/qrcode-ctrl.js
+++ app/views/components/conow-qrcode/qrcode-ctrl.js
@@ -1,35 +1,57 @@
 'use strict';
 
-app.controller('qrcodeCtrl', ['$scope', 
-	function($scope){
-		//
-		console.log('111111111111111111')
+app.controller('qrcodeCtrl', ['$scope', '$http', '$filter', 
+    function($scope, $http, $filter) {
+      $scope.qrcodeString = '';
+      
+      $http.get('views/components/conow-qrcode/data/qrcode.json')
+        .success(function(data) {
+          console.log(data);
 
-		var data = [ "BEGIN:VCARD",
-            "VERSION:3.0",
-            "N:Kent;Clark;W;;",
-            // "FN:Clark W Kent",
-            "ORG:Daily Planet",
-            // "TITLE:Senior Reporter",
-            "EMAIL;type=INTERNET;type=WORK:clark.kent@dailyplanet.com",
-            "TEL;type=CELL:800-555-1212",
-            "TEL;type=WORK:212-787-3762 x 6",
-            // "TEL;type=HOME:800-579-7866",
-            "ADR;type=WORK:;;One Daily Planet Way;Metropolis;NY;11111;USA",
-            // "ADR;type=HOME:;;One Jor El Place;Krypton City;Krypton;32.54323;Kryptonian Planetary Alliance",
-            // "BDAY;value=date:1932-01-01",
-            // "URL;type=WORK:http://www.dailyplanet.com",
-            // "URL;type=HOME:http://www.iamnotsuperman.com",
-            "END:VCARD" ];
-		var result = data.join(String.fromCharCode(13) + String.fromCharCode(10));
-console.log('result-->', result)
+          $scope.qrcodeObj = data.obj;
 
-		$scope.qrcodeString = result;
-	  $scope.size = 250;
-	  $scope.correctionLevel = '';
-	  $scope.typeNumber = 0;
-	  $scope.inputMode = '';
-	  $scope.image = true;
-		$scope.string = result;
-	}
+          $scope.qrcodeString = $filter('vcardFormat')(data.obj);
+        })
+        .error(function(msg) {
+          console.error(msg);
+        });
+
+        // $scope.qrcodeObj = {
+        //   "NAME": "胡一刀",
+        //   "ORG": "科南软件",
+        //   "TITLE": "软件开发",
+        //   "EMAIL": "hyd@conow.conow.cn",
+        //   "CELLPHONE": "13245798432",
+        //   "PHONE": "02032541789"
+        // };
+
+        // var data = ["BEGIN:VCARD",
+        //     "VERSION:3.0",
+        //     // "N:CHARSET=UTF-8:胡;一峰",
+        //     // "FN:Clark W Kent",
+        //     "FN:LANGUAGE=zh-CN:胡一峰",
+        //     //"ORG:CHARSET=UTF-8:科南软件",
+        //     // "TITLE:Senior Reporter",
+        //     "EMAIL;type=INTERNET;type=WORK:hyf@conow.cn",
+        //     "TEL;type=CELL:13245798432",
+        //     "TEL;type=WORK:121554545",
+        //     // "TEL;type=HOME:800-579-7866",
+        //     // "ADR;type=WORK:;;One Daily Planet Way;Metropolis;NY;11111;USA",
+        //     // "ADR;type=HOME:;;One Jor El Place;Krypton City;Krypton;32.54323;Kryptonian Planetary Alliance",
+        //     // "BDAY;value=date:1932-01-01",
+        //     // "URL;type=WORK:http://www.dailyplanet.com",
+        //     // "URL;type=HOME:http://www.iamnotsuperman.com",
+        //     "END:VCARD"
+        // ];
+        // var result = data.join(String.fromCharCode(13) + String.fromCharCode(10));
+        // console.log('result-->', result)
+        // result = '中国';
+        // $scope.qrcodeString = result;
+        // $scope.size = 250;
+        // $scope.correctionLevel = '';
+        // $scope.typeNumber = 0;
+        // $scope.inputMode = '';
+        // $scope.image = true;
+        // $scope.string = result;
+    }
 ]);
