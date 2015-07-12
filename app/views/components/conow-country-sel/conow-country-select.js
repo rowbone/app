c07be3830d2f7db21b2d9efe2352f8f0e04b7db9
wulujunwlj <wulujunwlj@163.com>
2015-07-12 16:49:53 +0800

    update country-sel

--- app/views/components/conow-country-sel/conow-country-select.js
+++ app/views/components/conow-country-sel/conow-country-select.js
@@ -1,7 +1,81 @@
 'use strict';
 
-app.directive('conowCountrySelect', ['DataService', 'conowModals', 
-	function(DataService, conowModals) {
+// countries data service
+app.service('countriesService', ['$q', 'DataService', 
+	function ($q, DataService) {
+		// 
+		var deferred = $q.defer();
+		var url = 'views/components/conow-country-sel/data/IC_COUNTRY.json'
+		var dataAll = [];
+		var self = this;
+		var options = {
+			isLoading: true
+		};
+
+		var init = function() {
+
+		}
+
+		// 获取所有国家的数据
+		this.getAllCountries = function() {
+
+			DataService.getData(url)
+				.then(function(data) {
+					self.dataAll = data;
+
+					options.isLoading = false;
+					deferred.resolve(data);
+				}, function(msg) {
+					self.dataAll = [];
+					options.isLoading = false;
+
+					deferred.reject('Get countries data wrong-->', msg);
+				});
+
+			return deferred.promise;
+		};
+
+		// data init
+		// self.getAllCountries();
+
+		// 根据 code 获取对应的已选项
+		var getSelectedFromData = function(keyCode, dataAll, codeName) {
+			var codeName = codeName;
+			var selected = null;
+			// var selected = [];
+			if(!codeName) {
+				codeName = 'code';
+			}
+			if(!dataAll) {
+				return;
+			}
+
+			var iLen = dataAll.length;
+			for(var i=0; i<iLen; i++) {
+				if(dataAll[i][codeName] === keyCode) {
+					return dataAll[i];
+				} else {
+					if(dataAll[i].children) {
+						return getSelectedFromData(keyCode, dataAll[i].children, codeName);
+					} else {
+						continue;						
+					}
+				}
+			}
+		};
+
+		this.getSelected = function(keyCode, dataAll, codeName) {
+			return getSelectedFromData(keyCode, dataAll, codeName);
+		}
+
+		// other methods
+
+	}
+]);
+
+// conow country select directive
+app.directive('conowCountrySelect', ['DataService', 'conowModals', 'countriesService', 
+	function(DataService, conowModals, countriesService) {
 
 		return {
 			restrict: 'AE',
@@ -15,7 +89,17 @@ app.directive('conowCountrySelect', ['DataService', 'conowModals',
 			replace: true,
 			template: '<input type="text" ng-click="countrySelClick($event)">',
 			link: function($scope, elem, attrs, ctrl) {
-				console.log('in link');
+				var selectedObj = null;
+		
+				var promise = countriesService.getAllCountries();
+				promise.then(function(data) {
+					selectedObj = countriesService.getSelected($scope.ngModel, data, 'OPTION_VALUE');
+
+					elem.val(selectedObj.OPTION_NAME);
+				}, function(msg) {
+					console.log('msg-->', msg);
+				})
+
 				var vm = $scope.vm = {};
 
 				$scope.countrySelClick = function(e) {
@@ -72,33 +156,6 @@ app.controller('countrySelCtrl', ['$scope', '$conowModalInstance', 'DataService'
 					selected: modalParams.selected
 				};
 
-		var getSelected = function(keyCode, dataAll, codeName) {
-			var codeName = codeName;
-			var selected = null;
-			// var selected = [];
-			if(!codeName) {
-				codeName = 'code';
-			}
-			if(!dataAll) {
-				return;
-			}
-
-			var iLen = dataAll.length;
-			for(var i=0; i<iLen; i++) {
-				if(dataAll[i][codeName] === keyCode) {
-					return dataAll[i];
-				} else {
-					if(dataAll[i].children) {
-						return getSelected(keyCode, dataAll[i].children, codeName);
-					} else {
-						continue;						
-					}
-				}
-			}
-		};
-
-		
-
 		var init = function() {
 			vm.dataCascade = [[], []];
 			vm.selected = [{}, {}];
@@ -111,9 +168,6 @@ app.controller('countrySelCtrl', ['$scope', '$conowModalInstance', 'DataService'
 					vm.dataAll = data;
 					vm.dataCascade[0] = data;
 
-					var code = '103';
-					console.log('111111111111111111111');
-					console.log(getSelected(code, vm.dataAll, 'OPTION_VALUE'));
 				}, function(msg) {
 					console.error('msg-->', msg);
 				})
@@ -133,6 +187,8 @@ app.controller('countrySelCtrl', ['$scope', '$conowModalInstance', 'DataService'
 					break;
 				case '2':
 					vm.selected[1] = item;
+
+					$scope.confirm();
 
 					break;
 			}
