ec8620ca0e4343d18b46486d391d0ac92cb2c21e
wulujunwlj <wulujunwlj@163.com>
2015-07-29 21:18:16 +0800

    table

--- app/views/components/conow-responsive-table/responsive-table-demo-ctrl.js
+++ app/views/components/conow-responsive-table/responsive-table-demo-ctrl.js
@@ -1,8 +1,9 @@
 'use strict';
 
-app.controller('responsiveTableDemoCtrl', ['$scope', 'ngTableParams', 
-	function($scope, ngTableParams) {
-		var data = [
+app.controller('responsiveTableDemoCtrl', ['$scope', 'ngTableParams', '$filter', '$timeout', '$interval', 
+	function($scope, ngTableParams, $filter, $timeout, $interval) {
+    
+		var data = $scope.data = [
       {name: "Moroni", age: 50},
       {name: "Tiancum", age: 43},
       {name: "Jacob", age: 27},
@@ -21,27 +22,39 @@ app.controller('responsiveTableDemoCtrl', ['$scope', 'ngTableParams',
       {name: "Nephi", age: 29},
       {name: "Enos", age: 34}
     ];
+    
+    
 
-    $scope.tableParams = new ngTableParams({
-        page: 1,            // show first page
-        count: 10,          // count per page
-        filter: {
-            name: 'M'     // initial sorting
-        }
-    }, {
-        total: data.length, // length of data
-        getData: function($defer, params) {
-            // use build-in angular filter
-            var orderedData = params.filter() ?
-                    $filter('filter')(data, params.filter()) :
-                    data;
+        $interval(function() {console.log('$timeout')
+          // $scope.users = [{name: "Moroni", age: 50}];
+          console.log('in reload...');
 
-            scope.users = orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
+          // data = [{name: '123', age: 8}];
+          $scope.tableParams.reload();
+        }, 3000);
 
-            params.total(orderedData.length); // set total for recalc pagination
-                $defer.resolve($scope.users);
-        }
-    });
+        $scope.tableParams = new ngTableParams({
+            page: 1,            // show first page
+            count: 10           // count per page
+        }, {
+            total: data.length, // length of data
+            getData: function($defer, params) {
+                $defer.resolve(data.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+            }
+        });
+
+        // $scope.$watch('$scope.$data', function(newVal, oldVal) {
+        //   console.log('newVal-->', newVal);
+        // }, true);
+
+        // $scope.userEditClick = function(user, e) {
+        //   e.preventDefault();
+
+        //   console.log(user);
+        //   user.$edit = !user.$edit;
+
+        //   e.stopPropagation();
+        // };
 
         
 	}
